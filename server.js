de667bc886356cb789902876d1658d326af3b754
Ching Chen <chingyg407@yahoo.com>
2016-12-24 11:26:20 -0500

    changed port back to 8080

--- server.js
+++ server.js
@@ -11,12 +11,4 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 
-app.listen(process.env.PORT || 5000);
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+app.listen(process.env.PORT || 8080);
