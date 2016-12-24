5825f27c25e5e93ccbc8ee73ef3a9122c78ac43f
Ching Chen <chingyg407@yahoo.com>
2016-12-24 10:59:21 -0500

    heroku  error fix?

--- server.js
+++ server.js
@@ -12,3 +12,11 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 
 app.listen(process.env.PORT || 5000);
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
